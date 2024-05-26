1d3b3c2788daa89a0d5d19d52bb0e4a0433aa98a
Bruno Sant'Anna <mail@bsantanna.me>
2024-05-26 23:00:34 +0200

    code cleanup

--- azure/learnmate-sweden-central/aks-learnmate-v1/env.sh
+++ azure/learnmate-sweden-central/aks-learnmate-v1/env.sh
@@ -5,9 +5,10 @@ export AZ_CLIENT_ID="?"
 export AZ_KEY_VAULT_NAME="key-vault-learnmate-v1"
 export AZ_KEY_VAULT_SOPS_KEY_NAME="aks-learnmate-v1-sops-key"
 export AZ_KEY_VAULT_SOPS_KEY_ID=$(\
-  az keyvault key show -n ${AZ_KEY_VAULT_SOPS_KEY_NAME} \
+  az keyvault key show \
+    -n ${AZ_KEY_VAULT_SOPS_KEY_NAME} \
     --vault-name ${AZ_KEY_VAULT_NAME} \
-    --query key.kid -o tsv\
+    --query key.kid -o tsv \
 )
 export AZ_RESOURCE_GROUP="learnmate-sweden-central"
 

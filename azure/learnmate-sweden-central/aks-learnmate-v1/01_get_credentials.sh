bf0c500fb9562f9abbd3e24392b7c14db2b343f0
Bruno Sant'Anna <mail@bsantanna.me>
2024-05-26 22:21:54 +0200

    aks utility shell scripts

---
+++ azure/learnmate-sweden-central/aks-learnmate-v1/01_get_credentials.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+source ./env.sh
+
+az aks get-credentials \
+  --name ${AZ_AKS_NAME}\
+  --resource-group ${AZ_RESOURCE_GROUP}
---
+++ azure/learnmate-sweden-central/aks-learnmate-v1/env.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+export AZ_AKS_NAME="aks-learnmate-v1"
+export AZ_CLIENT_ID="?"
+export AZ_KEY_VAULT_NAME="key-vault-learnmate-v1"
+export AZ_KEY_VAULT_SOPS_KEY_NAME="aks-learnmate-v1-sops-key"
+export AZ_KEY_VAULT_SOPS_KEY_ID=$(\
+  az keyvault key show -n ${AZ_KEY_VAULT_SOPS_KEY_NAME} \
+    --vault-name ${AZ_KEY_VAULT_NAME} \
+    --query key.kid -o tsv\
+)
+export AZ_RESOURCE_GROUP="learnmate-sweden-central"
+
+export DOCKER_REGISTRY_USERNAME="?"
+export DOCKER_REGISTRY_PASSWORD="?"

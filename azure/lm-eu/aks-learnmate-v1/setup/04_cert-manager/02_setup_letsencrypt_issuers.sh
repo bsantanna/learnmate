298ef3b1785e39fd5c33cebcc3bed77c352c541c
Bruno Sant'Anna <mail@bsantanna.me>
2024-06-01 19:24:50 +0200

    Adjusts cluster wide certificate issuers

--- azure/lm-eu/aks-learnmate-v1/setup/04_cert-manager/02_setup_letsencrypt_issuers.sh
+++ azure/lm-eu/aks-learnmate-v1/setup/04_cert-manager/02_setup_letsencrypt_issuers.sh
@@ -3,9 +3,7 @@
 source ../env.sh
 
 kubectl create \
-  -n "${AZ_AKS_APP_NAMESPACE}" \
   -f letsencrypt-staging-issuer.yaml
 
 kubectl create \
-  -n "${AZ_AKS_APP_NAMESPACE}" \
   -f letsencrypt-prod-issuer.yaml
